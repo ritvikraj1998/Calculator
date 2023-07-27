7478a751d8c96b0e1046266c9b1b103faf5d9d02
Ritvik Rajvanshi <ritvikraj1998@gmail.com>
2023-07-28 01:53:17 +0530

    asdasd

--- src/main/java/com/houarizegai/calculator/util/ColorUtil.java
+++ src/main/java/com/houarizegai/calculator/util/ColorUtil.java
@@ -17,4 +17,20 @@ public class ColorUtil {
                         Integer.valueOf(colorHex.substring(4, 6), 16)))
                 .orElse(null);
     }
+
+    class ColorUtil {
+
+        private ColorUtil() {
+            throw new AssertionError("Constructor is not allowed");
+        }
+
+        public static Color hex2Color(String colorHex) {
+            return Optional.ofNullable(colorHex)
+                    .map(hex -> new Color(
+                            Integer.valueOf(colorHex.substring(0, 2), 16),
+                            Integer.valueOf(colorHex.substring(2, 4), 16),
+                            Integer.valueOf(colorHex.substring(4, 6), 16)))
+                    .orElse(null);
+        }
+    }
 }
